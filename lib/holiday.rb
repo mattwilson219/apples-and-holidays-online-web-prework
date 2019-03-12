109866a54d5c7d796dd43afe040f9a58fc6aec82
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-12 18:46:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holidays, supplies|
-    puts  " {holidays.to_s.split("_").collect{|x| x.capitalize!}.join(" ")}: #{supplies.join(", ")  } "
-
+    puts  " {holidays.to_s.split("_").collect{|x| x.capitalize!}.join(" ")}: #{supplies.join(", ")} "
+  end  
+ end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
