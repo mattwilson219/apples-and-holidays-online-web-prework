5ebff614ede6e93536a2f0e4ce890384cfbb8266
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-12 19:05:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.collect do |season, holiday|  
-  
+  holiday.collect
 
 end
 
