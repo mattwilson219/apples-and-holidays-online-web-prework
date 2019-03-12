3c4fd7cf8ddf421fdb0706913b8b06c00c79c2fb
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-12 18:40:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holidays, supplies|
-      " "
+    puts  "holidays.to_s. "
 
 end
 
