09aceea9c2061feceedb1c335311252506647b9e
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-12 17:59:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,15 +28,15 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_supplies[:winter].each do |holiday, supplies|
     supplies << supply
   end  
-    
-
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_supplies[:spring].each do |holiday, supplies|
+    supplies << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
